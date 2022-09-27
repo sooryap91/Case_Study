6ac06f027919483501332f0885a72c23a94709ba
sooryap91 <sooryap1991@gmail.com>
2022-09-27 22:50:24 +0530

    changes

--- todolist.js
+++ todolist.js
@@ -1,6 +1,15 @@
 function ajax()
 {
     var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+    
+        if(request.readyState == 4 & request.status == 200){
+
+             var response = JSON.parse(this.responseText);
+           checkbox() 
+        }
+    };
     request.open("GET",'https://jsonplaceholder.typicode.com/todos');
-    request.send();
+
+    request.send()
 }
