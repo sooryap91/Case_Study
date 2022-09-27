0de6e91e84012664f8e5c26f175c4351b3b3b838
sooryap91 <sooryap1991@gmail.com>
2022-09-27 17:45:52 +0530

    changes

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-var email=document.getElementById("email");
-var password=document.getElementById("password");
-var error=document.getElementById("error");
-   function validate(){
 
-    if(email.value =="admin" && password.value =="12345") 
+    function validate(){
+        var email=document.getElementById("email");
+        var password=document.getElementById("password");
+        var error=document.getElementById("error");
+        if((email.value=="")||(password.value==""))
+            {
+            error.innerHTML="Fields cannot be empty";
+            error.style.color="red";
+            return false;
+           }
+    if((email.value=="admin")&&(password.value=="12345"))
     {
         error.innerHTML="valid";
         return true;
     }
     else {
-        error.alert("Username or Password Incorrect");
+        error.innerHTML="Invalid";
+        error.style.color="red";
         return false;
         }
-       
-}
+    }
   
